0d410eac57c0666d290cecb05db1e9363e5de092
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-07-25 20:40:14 +0530

    DSA: Array

---
+++ dsa/src/com/xworkz/dsa/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.xworkz.dsa;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={4,8,2,3,9};
+        int temp=0;
+        int minIndex=0;
+        System.out.println("before sorting: "+" ");
+        for (int ssss:arr) {
+            System.out.println(ssss);
+        }
+        for(int i=0;i<arr.length-1;i++){
+            minIndex=i;
+            for(int j=i+1;j<arr.length;j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            temp=arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+
+            System.out.println("after sorting: ");
+            for (int ssss:arr) {
+                System.out.println(ssss);
+            }
+        }
+
+    }
+}
