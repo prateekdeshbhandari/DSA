481f161d81ed9e9128a367c3dc71ce97c73214c1
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-08-11 19:33:24 +0530

    Optimized algorithm implementation

---
+++ dsa_quctions/src/com/xworkz/dsa/quctions/LeetCodeQ9.java
@@ -0,0 +1,18 @@
+package com.xworkz.dsa.quctions;
+
+public class LeetCodeQ9 {
+        public String convertDateToBinary(String date) {
+            String[] parts = date.split("-");
+
+            int year = Integer.parseInt(parts[0]);
+            int month = Integer.parseInt(parts[1]);
+            int day = Integer.parseInt(parts[2]);
+
+            String binaryYear = Integer.toBinaryString(year);
+            String binaryMonth = Integer.toBinaryString(month);
+            String binaryDay = Integer.toBinaryString(day);
+
+            return binaryYear + "-" + binaryMonth + "-" + binaryDay;
+        }
+    }
+
