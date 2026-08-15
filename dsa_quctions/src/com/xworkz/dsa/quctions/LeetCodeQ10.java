1f967fe2f9975f18f3a9b118c2c4ad31b6bb5469
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-08-15 19:30:36 +0530

    Optimized algorithm implementation

---
+++ dsa_quctions/src/com/xworkz/dsa/quctions/LeetCodeQ10.java
@@ -0,0 +1,24 @@
+package com.xworkz.dsa.quctions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCodeQ10 {
+
+        public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            // Find out the greatest number of candies among all the kids.
+            int maxCandies = 0;
+            for (int candy : candies) {
+                maxCandies = Math.max(candy, maxCandies);
+            }
+            // For each kid, check if they will have greatest number of candies
+            // among all the kids.
+            List<Boolean> result = new ArrayList<>();
+            for (int candy : candies) {
+                result.add(candy + extraCandies >= maxCandies);
+            }
+
+            return result;
+        }
+    }
+
