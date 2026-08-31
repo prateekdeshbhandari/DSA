4ccf268cfc423cc29526300d4763dcef044234db
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-08-31 21:41:10 +0530

    Optimized algorithm implementation

---
+++ dsa_quctions/src/com/xworkz/dsa/quctions/LeetCodeQ12.java
@@ -0,0 +1,22 @@
+package com.xworkz.dsa.quctions;
+
+import java.util.Arrays;
+
+public class LeetCodeQ12 {
+
+
+        public int[] transformArray(int[] nums) {
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]%2==0){
+                    nums[i]=0;
+
+                }else{
+                    nums[i]=1;
+                }
+
+            }
+            Arrays.sort(nums);
+            return nums;
+        }
+
+}
