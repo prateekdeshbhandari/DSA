ff4e60ffc24224eb19110d085b71e52e14f938e8
prateekdeshbhandari <prateekdeshbhandari@gmail.com>
2026-09-21 23:05:14 +0530

    Optimized algorithm implementation

---
+++ dsa_quctions/src/com/xworkz/dsa/quctions/LeetCodeQ16.java
@@ -0,0 +1,20 @@
+package com.xworkz.dsa.quctions;
+
+public class LeetCodeQ16 {
+
+        public int subtractProductAndSum(int n) {
+            int product = 1;
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+
+                product = product * digit;
+                sum = sum + digit;
+
+                n = n / 10;
+            }
+
+            return product - sum;
+        }
+    }
